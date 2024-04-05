391e5b34a6a202cd3a411dbf9c9401eded7c3ba2
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2024-04-05 14:09:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ estacionamientos = []
 with open('espacios.pkl', 'rb') as file:
     estacionamientos = pickle.load(file)
 
+# leer el video 
 video = cv2.VideoCapture('video.mp4')
 
 while True:
     check, img = video.read()
+    # escala de grises
     imgBN = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # más info en https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html
     imgTH = cv2.adaptiveThreshold(imgBN, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
     # más info en https://theailearner.com/tag/cv2-medianblur/
+    # filtro pasa baja
     imgMedian = cv2.medianBlur(imgTH, 5)
     kernel = np.ones((5,5), np.int8)
+    # dilatar las áreas o regiones de la imagen
     imgDil = cv2.dilate(imgMedian, kernel)
 
     for x, y, w, h in estacionamientos:
